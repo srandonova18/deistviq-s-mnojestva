181888740c2ca7716a6b262c95575aaf99da7a35
kvbolashikova18 <59692632+kvbolashikova18@users.noreply.github.com>
2020-10-22 21:44:47 +0300

    add function "sravnenie"

--- deistviq-s-mnojestva/deistviq-s-mnojestva.cpp
+++ deistviq-s-mnojestva/deistviq-s-mnojestva.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
 
+int* sravnenie(int a[], int n, int b[], int m, string sign, int* c, int& k) {
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i] == b[j]) {
+                count++;
+            }
+        }
+
+        if (sign == "==") {
+            if (count == 0)
+            {
+                c[k] = a[i];
+                k++;
+            }
+        }
+
+        else if (sign == "!=") {
+            if (count != 0)
+            {
+                c[k] = a[i];
+                k++;
+            }
+        }
+
+        count = 0;
+    }
+
+    return c;
+}
+
 int main() {
 
    
